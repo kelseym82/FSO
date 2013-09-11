d9d81400ee62866155ffd570f7c19f8059f10d05
kelseym82 <kelseym@fullsail.edu>
2013-09-11 15:48:23 -0400

    Finished JSON

--- Java1/Wk1/MagicEightBall/src/com/mkelsey/answers/JSON.java
+++ Java1/Wk1/MagicEightBall/src/com/mkelsey/answers/JSON.java
@@ -9,47 +9,51 @@ public class JSON {
 		
 		
 			// Create Lucky Day JSONOBject
-			JSONObject luckyDayObject = new JSONObject();
+			JSONObject horoscopeObject = new JSONObject();
 		try {		
 			// Create Query 
 			JSONObject queryObject = new JSONObject();
 			
 			//Create Answers in Query
-			for (Answers answer : Answers.values()){
+			for (Horoscope answer : Horoscope.values()){
 				//Create Object
 				JSONObject newObject = new JSONObject();
-				newObject.put("month", answer.setMonth());
-				newObject.put("numDays", answer.numDays());
+				//Adds Horoscope Text and Lucky Numbers
+				newObject.put("horoscopeText", answer.setHoroscope());
+				newObject.put("luckyNumber", answer.setLuckyNumber());
 				queryObject.put(answer.name().toString(), newObject);
 			}
 			// Add Query to LuckyDay
-			luckyDayObject.put("query", queryObject);
+			horoscopeObject.put("query", queryObject);
 			
 			
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
 		
-		return luckyDayObject;
+		return horoscopeObject;
 
 	}
 	
 		
 	
-	/*public static String readJSON ( String selected){
+	public static String readJSON ( String selected){
 		
-		String results, month, dayString;
-		Integer day;
+		String result, horoscopeText, luckyNumber;
 		JSONObject object = buildJSON();
 		
 		try {
-			month = object.getJSONObject("query").getJSONObject(selected).getString("month");
+			horoscopeText = object.getJSONObject("query").getJSONObject(selected).getString("horoscopeText");
+			luckyNumber = object.getJSONObject("query").getJSONObject(selected).getString("luckyNumber");
+			result = "Daily Horoscope: " + horoscopeText + "\r\n"
+					+ "Lucky Number: " + luckyNumber;
 			 
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			result = e.toString();
 		}
 		
-		return results;
-	}*/
+		return result;
+	}
 }
