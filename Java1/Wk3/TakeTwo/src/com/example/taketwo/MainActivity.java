72e404aee158b58bc4baadb225983c2a77e3e99b
kelseym82 <kelseym@fullsail.edu>
2013-09-25 09:47:03 -0400

    changed API to yahoo weather api

--- Java1/Wk3/TakeTwo/src/com/example/taketwo/MainActivity.java
+++ Java1/Wk3/TakeTwo/src/com/example/taketwo/MainActivity.java
@@ -2,11 +2,8 @@ package com.example.taketwo;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Context;
@@ -18,7 +15,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.mkelsey.lib.WebStuff;
 
@@ -28,15 +24,16 @@ public class MainActivity extends Activity {
 	Context _context;
 	LinearLayout _appLayout;
 	SearchForm _search;
-	BeerDisplay _beer;
+	BeerDisplay _weather;
 	SpinnerDisplay _spinner;
 	Boolean _connected = false;
-	String _beerName;
+	String _weatherName;
 	Integer _abv;
 	String _description;
 	ArrayList<String> _beerNameList = new ArrayList<String>();
 	ArrayList<String> _abvList = new ArrayList<String>();
 	ArrayList<String> _desList = new ArrayList<String>();
+
 
 
 	@Override
@@ -46,7 +43,7 @@ public class MainActivity extends Activity {
 		_context = this;
 		_appLayout = new LinearLayout(this);
 		
-		_search = new SearchForm(_context, "Enter Text", "Search");
+		_search = new SearchForm(_context, "Enter Zip Code", "Search");
 		
 		//EditText searchField = search.getField();
 		
@@ -58,7 +55,8 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				Log.i("CLICK HANDLER", _search.getField().getText().toString());
-				getBeerData(_search.getField().getText().toString());
+				getWeatherData(_search.getField().getText().toString());
+				
 			}
 		});
 		
@@ -69,14 +67,14 @@ public class MainActivity extends Activity {
 		}
 		
 		//Display Beer Data Handler
-		_beer = new BeerDisplay(_context);
+		_weather = new BeerDisplay(_context);
 		
 		//Spiiner Handler
 		_spinner = new SpinnerDisplay(_context);
 		
 		//Add view to the layout
 		_appLayout.addView(_search);
-		_appLayout.addView(_beer);
+		_appLayout.addView(_weather);
 		_appLayout.addView(_spinner);
 		
 		_appLayout.setOrientation(LinearLayout.VERTICAL);
@@ -90,19 +88,29 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	private void getBeerData(String beer){
-		String baseURL = "http://api.openbeerdatabase.com/v1/beers.json?callback=?";
+	private void getWeatherData(String zipCode){
+		String baseURL = "http://query.yahooapis.com/v1/public/yql?q=select%20item%20from%20weather.forecast%20where%20location%3D\""+ zipCode +"\"&format=json";
+		String qs;
+		try{
+			qs = URLEncoder.encode(baseURL, "UTF-8");
+		} catch (Exception e){
+			Log.e("BAD URL", "ENCODING PROBLEM");
+			qs = "";
+		}
 		URL finalURL;
 		try{
 			finalURL = new URL(baseURL);
-			beerDataRequest BDR = new beerDataRequest();
-			BDR.execute(finalURL);
-		} catch (MalformedURLException e){
+			Log.i("BASE URL =",baseURL);
+			WeatherRequest wr = new WeatherRequest();
+			wr.execute(finalURL);
+		} catch(MalformedURLException e) {
 			Log.e("BAD URL", "MALFORMED URL");
+			finalURL = null;
 		}
 	}
 	
-	private class beerDataRequest extends AsyncTask<URL, Void, String>{
+	
+	private class WeatherRequest extends AsyncTask<URL, Void, String>{
 		@Override
 		protected String doInBackground(URL... urls) {
 			String response = "";
@@ -116,21 +124,7 @@ public class MainActivity extends Activity {
 		protected void onPostExecute(String result){
 			Log.i("URL RESPONSE", result);
 			
-			try {
-					JSONObject json = new JSONObject(result);
-					if(json.getString("name").compareTo("")==00){
-						Toast toast = Toast.makeText(_context, "Invalid Beer Name", Toast.LENGTH_LONG);
-						toast.show();
-					}else{
-						Toast toast = Toast.makeText(_context, "Valid Beer Name", Toast.LENGTH_LONG);
-						toast.show();
-					}
-					
-				
-				
-			} catch (JSONException e) {
-				Log.e("JSON", "JSON OBJECT EXCEPTION");
-			}
+			
 		}
 		
 	}
